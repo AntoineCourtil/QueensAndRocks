7b054b02ddf059d9b86ecf02151c2fbbfa4c13cb
Simon Hajek <simon.hajek2@etu.univ-lorraine.fr>
2017-02-17 08:24:06 +0100

    init 2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import graphics.GameUI;
+
+/**
+ * Created by simon on 17/02/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        GameUI gui = new GameUI();
+        gui.launch();
+
+    }
+}
---
+++ src/gameElements/Game.java
@@ -0,0 +1,7 @@
+package gameElements;
+
+/**
+ * Created by simon on 17/02/17.
+ */
+public class Game {
+}
