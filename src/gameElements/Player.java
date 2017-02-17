7c8677afbcf45359037ef67ea9ea03d0b1e3e4ab
Antoine Courtil <courtil1u@fst-ats-312-l08.fst.site.univ-lorraine.fr>
2017-02-17 08:33:26 +0100

    Create Class Player

--- src/gameElements/Player.java
+++ src/gameElements/Player.java
@@ -4,4 +4,49 @@ package gameElements;
  * Created by simon on 17/02/17.
  */
 public class Player {
+	
+	private int number;
+	private String colorMode;
+	
+	public Player(int number){
+		this.number = number;
+		this.colorMode = "bw";
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getColorMode() {
+		return colorMode;
+	}
+
+	public void setColorMode(String colorMode) {
+		this.colorMode = colorMode;
+	}
+	
+	public String toString(){
+		
+		if(this.number == 0){
+			if(this.colorMode == "bw"){
+				return "white";
+			}
+			else{
+				return "green";
+			}
+		}
+		//colorMode == 1
+		else{
+			if(this.colorMode == "bw"){
+				return "black";
+			}
+			else{
+				return "orange";
+			}
+		}
+	}
 }
