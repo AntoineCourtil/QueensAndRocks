a52cc4e16d061ff6bb9d6ef2bdb896edeb97c85a
Simon Hajek <simon.hajek2@etu.univ-lorraine.fr>
2017-03-10 10:46:45 +0100

    chrono

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 /**
  * Created by simon on 17/02/17.
  */
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,8 +20,20 @@ public class Main {
 		board.setPiece(0, 0, new Queen(p00));
 
 		ArrayList<Board> alb = board.getSuccessors();
-
 		System.out.println(board.solutionSteps(board));
+
+		Date d = new Date();
+		long start_time  = d.getTime();
+		alb = board.depthFirstSearch(board);
+		long end_time = d.getTime();
+		System.out.println("--------------------EXECUTION TIME-----------------");
+		long execution_time = end_time-start_time;
+
+		//System.out.println(start_time);
+		//System.out.println(end_time);
+		System.out.println(execution_time+ " ms");
+
+
 
 		GameUI gui = new GameUI(board);
         gui.launch();
